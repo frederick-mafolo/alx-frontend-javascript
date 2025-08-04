27389c58b56b48afc0dbac1608b1e3aab9ef7ffa
Mankgarube Frederick <mfmafolo@outlook.com>
2025-08-04 18:56:42 +0200

    added class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -44,20 +44,16 @@ interface Teacher {
 
 
 
-
-  // Interface for the constructor
 interface StudentConstructor {
     new (firstName: string, lastName: string): StudentClassInterface;
   }
-  
-  // Interface for the class instance
   interface StudentClassInterface {
     workOnHomework(): string;
     displayName(): string;
   }
   
-  // Class implementation
-  class StudentClass  implements StudentClassInterface {
+
+  class StudentClass {
     private firstName: string;
     private lastName: string;
   
@@ -74,7 +70,6 @@ interface StudentConstructor {
       return this.firstName;
     }
   }
-  
   // Example usage
   const student = new StudentClass("John", "Doe");
   console.log(student.displayName()); // Output: "John"
